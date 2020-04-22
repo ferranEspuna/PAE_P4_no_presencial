45e97e80782bdcbf80989240cd88ae6c6776c915
gerardPerello <perello999@gmail.com>
2020-04-22 13:42:35 +0200

    klk

--- dyn/dyn_app_motors.c
+++ dyn/dyn_app_motors.c
@@ -72,7 +72,7 @@ int robotMoveContinuous(int16_t speed){
 
     //si anem cap endavant (sign és true), volem que la roda esquerra giri en sentit antihorari, i, si és false, horari
     int speedLeft = dyn_setTurnSpeed(ID_MOTOR_LEFT, absSpeed, !sign);
-    //si anem cap endavant (sign és true), volem que la roda esquerra giri en sentit horari, i, si és false, antihorari
+    //si anem cap endavant (sign és true), volem que la roda dreta giri en sentit horari, i, si és false, antihorari
     int speedRight = dyn_setTurnSpeed(ID_MOTOR_RIGHT, absSpeed, sign);
 
     //Creem un array amb els returns de les funcions bàsiques.
@@ -105,4 +105,97 @@ int dyn_readTurnSpeed(uint8_t id, uint16_t *speed, bool *direction){
 
     //si hi ha hagut algun error al llegir, retornem 1. Si no, retornem 0.
     return (read1 > 0) | (read2 > 0);
+}
+
+int dyn_readTurnContinuous(uint8_t id, bool *continuous){
+
+
+}
+
+int robotSpinContinuous(int16_t speed){
+
+    //Si li passem el valor 0, aturem el robot.
+    if(speed == 0){return robotStop();}
+
+    //Posem els mòduls en mode turnContinuous
+    int setupLeft = dyn_turnContinuous(ID_MOTOR_LEFT);
+    int setupRight = dyn_turnContinuous(ID_MOTOR_RIGHT);
+
+    //Mirem el signe de la velocitat per a saber en quina direcció hem de girar les rodes
+    bool sign = (speed > 0);
+
+    //trobem el valor absolut de la velocitat.
+    uint16_t absSpeed = abs(speed);
+
+    //Si la velocitat és més gran que la màxima, posem els motors a màxima velocitat.
+    if(absSpeed > DYN_MAX_SPEED){absSpeed = DYN_MAX_SPEED;}
+
+    //Fem les operacions per a canviar la velocitat i direcció dels motors
+
+    //Si girem sobre nosaltres mateixos en sentit horari (sign és true), volem que la roda esquerra giri en sentit antihorari, i, si és false, horari
+    int speedLeft = dyn_setTurnSpeed(ID_MOTOR_LEFT, absSpeed, !sign);
+    //Si girem sobre nosaltres mateixos en sentit horari (sign és true), volem que la roda dreta giri en sentit antihorari, i, si és false, horari
+    int speedRight = dyn_setTurnSpeed(ID_MOTOR_RIGHT, absSpeed, !sign);
+
+    //Creem un array amb els returns de les funcions bàsiques.
+    int returns[4] = {setupLeft, setupRight, speedLeft, speedRight};
+
+    //Si alguna ha donat algun error (return major que 0), el retornem.
+    for(int i = 0; i < 4; i++){
+        if(returns[i] > 0){return returns[i];}
+    }
+
+    //Si no hi ha hagut cap error, retornmem 0.
+    return 0;
+}
+
+int moveSideContinuous(int16_t speed, int side){
+    //Si li passem el valor 0, aturem el robot.
+    if(speed == 0){return robotStop();}
+
+    //Posem els mòduls en mode turnContinuous
+    int setupLeft = dyn_turnContinuous(ID_MOTOR_LEFT);
+    int setupRight = dyn_turnContinuous(ID_MOTOR_RIGHT);
+
+    //Mirem el signe de la velocitat per a saber en quina direcció hem de girar les rodes
+    bool sign = (speed > 0);
+
+    //trobem el valor absolut de la velocitat.
+    uint16_t absSpeed = abs(speed);
+
+    //Si la velocitat és més gran que la màxima, posem els motors a màxima velocitat.
+    if(absSpeed > DYN_MAX_SPEED){absSpeed = DYN_MAX_SPEED;}
+
+    //Fem les operacions per a canviar la velocitat i direcció dels motors, mirant primer de tot el costat
+    //cap on volem anar. Si el side es positiu, ESQUERRA, per tant haurem de reduir a la meitat la velocitat
+    // de la roda de la l'esquerra. Si el side es negatiu, DRETA, per tant haurem de reduir a la meitat la
+    //velocitat de la roda de la dreta.
+    int speedLeft = 0;
+    int speedRight = 0;
+    if(side == 1){
+        //ESQUERRA.
+        //Si anem cap endavant (sign és true), volem que la roda esquerra giri en sentit antihorari, i, si és false, horari
+        speedLeft = dyn_setTurnSpeed(ID_MOTOR_LEFT, absSpeed/2, !sign);
+        //Si anem cap endavant (sign és true), volem que la roda dreta giri en sentit horari, i, si és false, antihorari
+        speedRight = dyn_setTurnSpeed(ID_MOTOR_RIGHT, absSpeed, sign);
+    }else{
+        //DRETA
+        //Si anem cap endavant (sign és true), volem que la roda esquerra giri en sentit antihorari, i, si és false, horari
+        speedLeft = dyn_setTurnSpeed(ID_MOTOR_LEFT, absSpeed, !sign);
+        //Si anem cap endavant (sign és true), volem que la roda dreta giri en sentit horari, i, si és false, antihorari
+        speedRight = dyn_setTurnSpeed(ID_MOTOR_RIGHT, absSpeed/2, sign);
+    }
+
+
+
+    //Creem un array amb els returns de les funcions bàsiques.
+    int returns[4] = {setupLeft, setupRight, speedLeft, speedRight};
+
+    //Si alguna ha donat algun error (return major que 0), el retornem.
+    for(int i = 0; i < 4; i++){
+        if(returns[i] > 0){return returns[i];}
+    }
+
+    //Si no hi ha hagut cap error, retornmem 0.
+    return 0;
 }
